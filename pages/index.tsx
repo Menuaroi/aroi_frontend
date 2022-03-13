76f39375465e6592edf2b6cdb11cda85737b391b
Nara <narawit.tub@outlook.co.th>
2022-03-13 16:55:33 +0700

    :lipstick: (login) add html elements for login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+export default function Login() {
+  return (
+    <>
+      <h1 className="">login</h1>
+      <p>Email: </p>
+      <p>Password: </p>
+      <button role="button" name="Submit user">
+        login
+      </button>
+    </>
+  );
 }
