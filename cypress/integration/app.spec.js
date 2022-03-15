368c8cb1df88ba0f201feb256cf38a4ad459ccf4
Nara <narawit.tub@outlook.co.th>
2022-03-15 23:49:21 +0700

    :green_heart: (Cypress) Fix Cypress run failed on local

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -4,6 +4,6 @@ describe("Navigation", () => {
     cy.visit("http://localhost:3000/");
 
     // The new page should contain an h1 with "About page"
-    cy.get("h1").contains("Hello world!");
+    cy.get("h1").contains("Homepage");
   });
 });
